8588056ef3247a4e11e3ba348876af183d50ae49
shinhj <shinheejoon@sinhuijun-ui-MacBookPro.local>
2021-01-02 21:32:56 +0900

    event

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
 import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return(
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-
-  }
-}
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mode:'read',
+      selected_contend_id:2,
+      subject:{title:"WEB", sub:"world wide web!"},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:1, title:'CSS', desc:'CSS is for design'},
+        {id:1, title:'JavaScript', desc:'JavaScript is for interactive'}
+
+      ]
+    }
+  }
   render () {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_contend_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i += 1;
+      }
+    }
     return (
-    
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}>
+        </Subject>
+        <TOC
+          onChangePage = {function(id){
+            this.setState({
+              mode:'read',
+              selected_contend_id:Number(id)
+          });
+        }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
